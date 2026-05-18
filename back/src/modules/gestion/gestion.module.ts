a96abc315f9372d110e8f508028516c202a0369f
SantiagoCarro <santiagoo9513@gmail.com>
2026-05-17 21:07:20 -0300

    agrego los modulos de proyectos

--- back/src/modules/gestion/gestion.module.ts
+++ back/src/modules/gestion/gestion.module.ts
@@ -7,12 +7,14 @@ import { AsignacionProyecto } from './entities/asignacion-proyecto.entity';
 import { Usuario } from '../auth/entities/usuario.entity';
 import { ClientesController } from './controllers/clientes.controller';
 import { AsignacionesController } from './controllers/asignaciones.controller';
+import { ProyectosController } from './controllers/proyectos.controller'; 
 import { ClientesService } from './services/clientes.service';
 import { AsignacionesService } from './services/asignaciones.service';
+import { ProyectosService } from './services/proyectos.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Cliente, Proyecto, AsignacionProyecto, Usuario]), AuthModule],
-  controllers: [ClientesController, AsignacionesController],
-  providers: [ClientesService, AsignacionesService],
+  controllers: [ClientesController, AsignacionesController, ProyectosController],
+  providers: [ClientesService, AsignacionesService, ProyectosService],
 })
 export class GestionModule {}
